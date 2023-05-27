40e97f9c86c740c55a9abf70991201a832664f68
Jorge Olivé <j.olive.rodriguez@gmail.com>
2023-05-27 11:29:54 +0200

    Nginx

--- NetCoreConf2023.BlogApp.IntegrationTests/Infrastructure.cs
+++ NetCoreConf2023.BlogApp.IntegrationTests/Infrastructure.cs
@@ -7,79 +7,78 @@ using Npgsql;
 using Testcontainers.PostgreSql;
 using Testcontainers.Redis;
 
-namespace NetCoreConf2023.BlogApp.IntegrationTests
+namespace NetCoreConf2023.BlogApp.IntegrationTests;
+
+public class RedisWaitStrategy : IWaitUntil
 {
-    public class RedisWaitStrategy : IWaitUntil
+    public async Task<bool> UntilAsync(IContainer container)
     {
-        public async Task<bool> UntilAsync(IContainer container)
-        {
-            var response = await container.ExecAsync(new List<string> { "redis-cli", "ping" });
-            return response.Stdout.Contains("PONG");
-        }
+        var response = await container.ExecAsync(new List<string> { "redis-cli", "ping" });
+        return response.Stdout.Contains("PONG");
     }
-    public class Infrastructure : IAsyncLifetime
+}
+public class Infrastructure : IAsyncLifetime
+{
+    private IFutureDockerImage _nginxImage;
+    private INetwork _dockerNetwork;
+    public RedisContainer Redis;
+    public PostgreSqlContainer PostgreSql;
+
+    public IContainer NginxContainer { get; }
+
+    public Infrastructure()
     {
-        private INetwork _dockerNetwork;
-        public RedisContainer Redis;
-        public PostgreSqlContainer PostgreSql;
-        private IFutureDockerImage nginxImage;
+        _dockerNetwork = new NetworkBuilder().Build(); //Network not neccesary, only for demonstration purposes
 
-        public IContainer NginxContainer { get; }
+        Redis = new RedisBuilder().
+            WithImage("redis:latest"). // RedisTestContainer defaults to v7
+            WithNetwork(_dockerNetwork).
+            WithWaitStrategy(
+                Wait.ForUnixContainer().AddCustomWaitStrategy(new RedisWaitStrategy())). // Not neccesary, only for demonstration purposes
+            Build();
 
-        public Infrastructure()
+        PostgreSql = new PostgreSqlBuilder().
+            WithNetwork(_dockerNetwork).     // No need to provide WaitStrategy, see PostgresqlContainer internals
+            Build();
+
+        _nginxImage = new ImageFromDockerfileBuilder().
+            WithDockerfileDirectory(Directory.GetCurrentDirectory()).
+            WithDockerfile("Dockerfile").
+            Build();
+
+        NginxContainer = new ContainerBuilder().WithImage(_nginxImage).WithPortBinding(80, true).Build();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _nginxImage.CreateAsync();
+
+        await Redis.StartAsync();
+        await PostgreSql.StartAsync();
+        await NginxContainer.StartAsync();
+    }
+
+    public async Task Reset()
+    {
+        await Redis.ExecAsync(new List<string> { "redis-cli", "FLUSHALL" });
+        await ResetPostgresql();
+    }
+
+    private async Task ResetPostgresql()
+    {
+        using (var connection = new NpgsqlConnection(PostgreSql.GetConnectionString()))
         {
-            _dockerNetwork = new NetworkBuilder().Build(); //Network not neccesary, only for demonstration purposes
+            await connection.OpenAsync();
 
-            Redis = new RedisBuilder().
-                WithImage("redis:latest"). // RedisTestContainer defaults to v7
-                WithNetwork(_dockerNetwork).
-                WithWaitStrategy(
-                    Wait.ForUnixContainer().AddCustomWaitStrategy(new RedisWaitStrategy())). // Not neccesary, only for demonstration purposes
-                Build();
+            // Poorman's reset: Execute SQL commands to drop and recreate the database
+            var dropDatabaseCommand = new NpgsqlCommand(@"DELETE FROM  public.""Blogs"";", connection);
 
-            PostgreSql = new PostgreSqlBuilder().
-                WithNetwork(_dockerNetwork).     // No need to provide WaitStrategy, see PostgresqlContainer internals
-                Build();
-
-            nginxImage = new ImageFromDockerfileBuilder().
-                WithDockerfileDirectory(Directory.GetCurrentDirectory()).
-                WithDockerfile("Dockerfile").
-                Build();
-
-             NginxContainer = new ContainerBuilder().WithImage(nginxImage).WithPortBinding(80, true).Build();
-        }
-
-        public Task DisposeAsync()
-        {
-            return Task.CompletedTask;
-        }
-
-        public async Task InitializeAsync()
-        {
-            await nginxImage.CreateAsync();
-
-            await Redis.StartAsync();
-            await PostgreSql.StartAsync();
-            await NginxContainer.StartAsync();
-        }
-
-        public async Task Reset()
-        {
-            await Redis.ExecAsync(new List<string> { "redis-cli", "FLUSHALL" });
-            await ResetPostgresql();
-        }
-
-        private async Task ResetPostgresql()
-        {
-            using (var connection = new NpgsqlConnection(PostgreSql.GetConnectionString()))
-            {
-                await connection.OpenAsync();
-
-                // Poorman's reset: Execute SQL commands to drop and recreate the database
-                var dropDatabaseCommand = new NpgsqlCommand(@"DELETE FROM  public.""Blogs"";", connection);
-
-                await dropDatabaseCommand.ExecuteNonQueryAsync();
-            }
+            await dropDatabaseCommand.ExecuteNonQueryAsync();
         }
     }
 }
